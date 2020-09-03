5ea7f8e55291b827dd75ff94f56aee91aa931c29
unknown <gipek03@gmail.com>
2020-09-03 04:07:17 +0300

    css refactor

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,18 +1,24 @@
 import React from 'react';
+import { connect } from "react-redux";
+import { get } from 'lodash';
+
 import Search from '../Search/Search';
 import Logo from './../Logo/Logo';
 
 import './Home.scss';
 
-const Home = () => {
-
+const Home = ({searchResults}) => {
     return (
-        <div className="wrapper">
-            <Logo />
-            <Search />
+        <div className= {`wrapper ${searchResults.length > 0  &&  'top'}`} wrapper>
+            <div className="container">
+                <Logo />
+                <Search />
+            </div>
         </div>
     )
 }
 
-
-export default Home;
+const mapStateToProps = ({data}) => ({ 
+    searchResults: get(data, 'searchResults.items', [])
+});
+export default connect(mapStateToProps, null)(Home);
